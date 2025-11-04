2800a04b007be2035112bf9d3f2ee837a7c42422
RiccardoParrino <riccardoparrino1@gmail.com>
2025-11-04 16:00:11 +0100

    adding other annotation

--- classLoaderExample/controller/MyController.java
+++ classLoaderExample/controller/MyController.java
@@ -1,7 +1,9 @@
 package classLoaderExample.controller;
 
 import classLoaderExample.annotations.Bean;
+import classLoaderExample.annotations.GetMapping;
 import classLoaderExample.annotations.Inject;
+import classLoaderExample.annotations.PostMapping;
 import classLoaderExample.model.Order;
 import classLoaderExample.service.MyService;
 
@@ -11,10 +13,12 @@ public class MyController {
     @Inject
     private MyService myService;
 
+    @PostMapping(name = "createOrder")
     public void createOrder(Order order) {
         this.myService.addOrder(order);
     }
 
+    @GetMapping(name = "printOrders")
     public void printOrder() {
         this.myService.getOrders().forEach(s -> System.out.println(s.getName()));
     }
